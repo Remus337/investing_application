f72248b6818bac68b81a11e10c7c076c16ec7dd9
Volodymyr Palazanov <volodia7775@gmail.com>
2024-04-19 17:56:24 +0200

    added ability to fetch stock data for a range of time

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const { getStockPrice } = require('./routes/stocks');
+const { getStockPricesForDateRange } = require('./routes/stocks');
 
 const app = express();
 const port = 3001;
 
-app.get('/stock/:symbol', async (req, res) => {
+app.get('/stock/:symbol/range/:startDate/:endDate', async (req, res) => {
+    const { symbol, startDate, endDate } = req.params;
     try {
-        const stockPrice = await getStockPrice(req.params.symbol);
-        res.json(stockPrice);
+        const stockData = await getStockPricesForDateRange(symbol, startDate, endDate);
+        res.json(stockData);
     } catch (error) {
-        res.status(500).send('Error fetching stock price');
+        console.error(`Error while fetching data for ${symbol}: ${error.message}`);
+        res.status(500).send('Error fetching stock prices');
     }
 });
 
--- server/src/routes/stocks.js
+++ server/src/routes/stocks.js
@@ -2,22 +2,34 @@ const axios = require('axios');
 
 const POLYGON_API_KEY = 'ArRNBD4RwUZFA5kE1Zu7IZiTwbFaHJl4';
 const BASE_URL = 'https://api.polygon.io';
+const symbols = ['AAPL', 'MSFT', 'GOOGL'];
 
-async function getStockPrice(symbol) {
+async function getStockPricesForDateRange(symbol, startDate, endDate) {
+    const url = `${BASE_URL}/v2/aggs/ticker/${symbol}/range/1/day/${startDate}/${endDate}`;
     try {
-        const url = `${BASE_URL}/v2/aggs/ticker/${symbol}/range/1/day/2024-01-09/2024-01-09`;
         const response = await axios.get(url, {
             params: {
                 apiKey: POLYGON_API_KEY
             }
         });
-
         return response.data;
     } catch (error) {
-        console.error(`Error fetching stock price for ${symbol}:`, error.message);
-        
+        console.error(`Error fetching stock prices for ${symbol}:`, error.message);
         throw error;
     }
 }
 
-module.exports = { getStockPrice };
+async function fetchAllStocksData(symbols, startDate, endDate) {
+    for (const symbol of symbols) {
+        try {
+            const data = await getStockPricesForDateRange(symbol, startDate, endDate);
+            console.log(`Data for ${symbol}:`, data);
+        } catch (error) {
+            console.error(`Failed to fetch data for ${symbol}:`, error);
+        }
+    }
+}
+
+fetchAllStocksData(symbols, '2022-01-01', '2024-01-01');
+
+module.exports = { getStockPricesForDateRange, fetchAllStocksData };
